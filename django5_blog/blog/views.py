dfa2798d74f57064c3c8131fb54934c03ca00acd
myasoedas <myasoedas@yandex.ru>
2024-12-08 18:29:18 +0300

    Доработал дизайн, если автор не загружает свое фото - подставляется фото по умолчанию

--- django5_blog/blog/views.py
+++ django5_blog/blog/views.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render
-from django.utils.html import escape
 from .models import Post
+from django.conf import settings
+
+
+def get_image_url(image_field, request):
+    """
+    Возвращает абсолютный URL изображения или URL изображения по умолчанию.
+    """
+    if image_field and image_field.name:  # Проверка наличия имени файла
+        return request.build_absolute_uri(image_field.url)
+    return request.build_absolute_uri(settings.STATIC_URL + "img/default-og-image.jpg")
 
 
 def post_list(request):
@@ -10,10 +19,10 @@ def post_list(request):
     context = {
         'posts': posts,
         'page_title': "Список публикаций",
-        'page_description': escape("Читайте последние публикации на нашем блоге. Узнайте больше на интересные темы."),
+        'page_description': "Читайте последние публикации на нашем блоге. Узнайте больше на интересные темы.",
         'page_og_author': "Администратор",
-        'page_og_image_url': request.build_absolute_uri("/static/img/default-og-image.jpg"),
-        'page_og_image_alt': escape("Общее изображение для страницы списка публикаций"),
+        'page_og_image_url': get_image_url(None, request),  # Используем изображение по умолчанию
+        'page_og_image_alt': "Общее изображение для страницы списка публикаций",
         'page_og_site_name': "Мой блог",
         'page_og_locale': "ru_RU",
         'page_robots': "index, follow",
@@ -28,17 +37,14 @@ def post_detail(request, id):
         id=id,
         status=Post.Status.PUBLISHED
     )
-    og_image_url = (
-        request.build_absolute_uri(post.image.url)
-        if post.image else request.build_absolute_uri("/static/img/default-og-image.jpg")
-    )
+    og_image_url = get_image_url(post.image, request)
     context = {
         'post': post,
-        'page_title': escape(post.title),
-        'page_description': escape(post.body[:160]),  # Ограничение описания до 160 символов
+        'page_title': post.title,
+        'page_description': post.body[:160],  # Ограничение описания до 160 символов
         'page_og_author': post.author.username,
         'page_og_image_url': og_image_url,
-        'page_og_image_alt': escape(f"Изображение для публикации {post.title}"),
+        'page_og_image_alt': f"Изображение для публикации {post.title}",
         'page_og_site_name': "Мой блог",
         'page_og_locale': "ru_RU",
         'page_robots': "index, follow",
@@ -50,15 +56,16 @@ def author_posts(request, author_id):
     """Публикации конкретного автора."""
     author = get_object_or_404(User, id=author_id)
     posts = Post.published.filter(author=author)
-    og_image_url = request.build_absolute_uri("/static/img/default-og-image.jpg")  # Замените на URL аватара, если доступен
+    # Получаем изображение автора или используем изображение по умолчанию
+    og_image_url = get_image_url(posts[0].image if posts.exists() else None, request)
     context = {
         'author': author,
         'posts': posts,
-        'page_title': escape(f"Публикации автора {author.username}"),
-        'page_description': escape(f"Список публикаций, созданных пользователем {author.username}."),
+        'page_title': f"Публикации автора {author.username}",
+        'page_description': f"Список публикаций, созданных пользователем {author.username}.",
         'page_og_author': author.username,
         'page_og_image_url': og_image_url,
-        'page_og_image_alt': escape(f"Изображение профиля автора {author.username}"),
+        'page_og_image_alt': f"Изображение профиля автора {author.username}",
         'page_og_site_name': "Мой блог",
         'page_og_locale': "ru_RU",
         'page_robots': "index, follow",
--- django5_blog/django5_blog/urls.py
+++ django5_blog/django5_blog/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 
 urlpatterns = [
@@ -7,3 +9,7 @@ urlpatterns = [
     path('blog/', include('blog.urls', namespace='blog')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
